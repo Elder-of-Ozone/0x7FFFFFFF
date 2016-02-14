85dd4f8bfccfcfc97b270082655d4a6179b3fffd
elder-of-ozone <a.fordham@iinet.net.au>
2016-02-14 16:37:56 +0800

    adding static and media

--- _0x7FFFFFFF/settings.py
+++ _0x7FFFFFFF/settings.py
@@ -102,8 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIR = (
-            os.path.join(BASE_DIR, 'static'),
+            os.path.join(BASE_DIR, 'static/'),
             
            # '/home/hydrius/Documents/0x7FFFFFFF/_0x7FFFFFFF',
             )
