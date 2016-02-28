ad1e669ff1ce3a588f970a507974874898254623
elder-of-ozone <a.fordham@iinet.net.au>
2016-02-28 20:38:04 +0800

    renamed app

--- _0x7FFFFFFF/urls.py
+++ _0x7FFFFFFF/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'Home.views.index.page')
+    url(r'^$', "blog.views.index", name='index'),
+    url(r'^about/', "blog.views.about", name='about'),
+    url(r'^Fitbit/', "blog.views.fitbit", name='fitbit')
 ]
